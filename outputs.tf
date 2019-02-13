765b9585910b7da7f2c024e8e939c84947a9db2e
Dave Murphy <davemurphy@newrelic.com>
2019-02-12 17:39:42 -0800

    Make outputs more meaningful

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "ip_address" {
+output "bastion_ip_address" {
+  value = "${azurerm_public_ip.bastion-pip.ip_address}"
+}
+
+output "app_ip_address" {
   value = "${azurerm_public_ip.app-pip.ip_address}"
 }
